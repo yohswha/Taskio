ed238a50813dfbc23c8fe19bf2cb748d89181775
yohswha <yohswhaenmanuel.com>
2025-08-20 10:28:14 -0400

    login updates

--- app/src/main/java/com/example/taskio/view/LoginActivity.java
+++ app/src/main/java/com/example/taskio/view/LoginActivity.java
@@ -17,8 +17,8 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        emailInput = findViewById(R.id.emailInput);
-        passwordInput = findViewById(R.id.passwordInput);
+        emailInput = findViewById(R.id.etEmail);
+        passwordInput = findViewById(R.id.etPassword);
         loginBtn = findViewById(R.id.btnLogin);
 
         loginBtn.setOnClickListener(v -> {
@@ -32,7 +32,7 @@ public class LoginActivity extends AppCompatActivity {
                 Toast.makeText(this, "Login successful", Toast.LENGTH_SHORT).show();
 
                 // Example: Navigate to AddTaskActivity
-                Intent intent = new Intent(LoginActivity.this, AddTaskActivity.class);
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                 startActivity(intent);
                 finish();
             }
